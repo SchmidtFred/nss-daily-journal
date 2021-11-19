1f2e43bcb7aa1f188b566cf5dab56c5a467adf17
Seracrooce <fschmidt@live.com>
2021-11-19 16:02:26 -0600

    moved the form creation to a js module and adjusted the render functions

--- scripts/dailyJournal.js
+++ scripts/dailyJournal.js
@@ -1,9 +1,17 @@
 import { entries } from "./entries.js";
+import { createEntryForm } from "./journalForm.js";
 
 export const dailyJournal = () => {
-    return `
+	return `
+        <h1>Daily Journal</h1>
+
+        <article class="entryForm">
+        ${createEntryForm()}
+       </article>
+
+
         <div class="entryList">
             ${entries()}
             </div>
-    `
-}
+    `;
+};
